80e8525333b85471fbe82777a7d82993f61b5ebf
LeSean Bruneau <anyone@hotmai.com>
2019-08-10 08:02:09 -0500

    Saratoga 2019 - Day 20

--- R-scripts/SARResultsUtils.R
+++ R-scripts/SARResultsUtils.R
@@ -13,3 +13,24 @@
 #sarRaceResults <- read.table("~/saratoga-data/csv/all-2019-SAR-RaceResults.txt", sep=",", header=TRUE, stringsAsFactors = TRUE)
 
 
+postTimeFavs <- function(races) {
+  ptFavResults <- data.frame(matrix(ncol = 6))
+  names(ptFavResults) <- c("races", "first", "second", "third", "fourth", "other")
+  totalRaces <- subset(races, (races$NUMHORSES > 3))
+  ptWin <- subset(totalRaces, (totalRaces$PTFAV1 == 'TRUE'))
+  ptPlace <- subset(totalRaces, (totalRaces$PTFAV2 == 'TRUE'))
+  ptShow <- subset(totalRaces, (totalRaces$PTFAV3 == 'TRUE'))
+  ptFourth <- subset(totalRaces, (totalRaces$PTFAV4 == 'TRUE'))
+  ptOther <- subset(totalRaces, ((totalRaces$PTFAV1 == 'FALSE') & (totalRaces$PTFAV2 == 'FALSE')
+                            & (totalRaces$PTFAV3 == 'FALSE') & (totalRaces$PTFAV4 == 'FALSE'))) 
+  
+  ptFavResults[1,1] <- nrow(totalRaces)
+  ptFavResults[1,2] <- round(nrow(ptWin) / nrow(totalRaces), digits = 3) 
+  ptFavResults[1,3] <- round(nrow(ptPlace) / nrow(totalRaces), digits = 3)
+  ptFavResults[1,4] <- round(nrow(ptShow) / nrow(totalRaces), digits = 3)
+  ptFavResults[1,5] <- round(nrow(ptFourth) / nrow(totalRaces), digits = 3)
+  ptFavResults[1,6] <- round(nrow(ptOther) / nrow(totalRaces), digits = 3)
+  
+  return (ptFavResults)
+}
+
